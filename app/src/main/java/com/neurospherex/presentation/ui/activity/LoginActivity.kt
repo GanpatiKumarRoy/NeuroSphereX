9613736beda2bbe16a15a562b98a4427e49798a5
110676 <Ecom@123>
2024-10-04 14:28:18 +0530

    Committing the project

---
+++ app/src/main/java/com/neurospherex/presentation/ui/activity/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.neurospherex.presentation.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.neurospherex.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_login)
+    }
+}
