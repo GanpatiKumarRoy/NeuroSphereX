0da87ab148c56f95547a90b128794204579f1ca5
110676 <Ecom@123>
2024-10-05 11:30:57 +0530

    Changes in Status bar

--- app/src/main/java/com/neurospherex/presentation/ui/activity/LoginActivity.kt
+++ app/src/main/java/com/neurospherex/presentation/ui/activity/LoginActivity.kt
@@ -1,13 +1,30 @@
 package com.neurospherex.presentation.ui.activity
 
+import android.os.Build
 import android.os.Bundle
+import android.view.View
+import android.view.WindowInsetsController
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
 import com.neurospherex.R
+import com.neurospherex.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var activityLoginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        activityLoginBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        activityLoginBinding.lifecycleOwner = this
+
+        window.statusBarColor = ContextCompat.getColor(this, R.color.background_color)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.setSystemBarsAppearance(WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS, WindowInsetsController.APPEARANCE_LIGHT_STATUS_BARS)
+        } else {
+            @Suppress("DEPRECATION")
+            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
+        }
     }
 }
