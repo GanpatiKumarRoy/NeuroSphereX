15f276b897792b6f90334d258a49a1d9c6258151
110676 <Ecom@123>
2024-10-20 21:49:46 +0530

    Working on side drawer.

--- app/src/main/java/com/neurospherex/presentation/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/neurospherex/presentation/ui/activity/HomeActivity.kt
@@ -1,19 +1,25 @@
 package com.neurospherex.presentation.ui.activity
 
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.material.navigation.NavigationView
 import com.neurospherex.R
 import com.neurospherex.data.remote.models.Course
 import com.neurospherex.databinding.ActivityHomeBinding
 import com.neurospherex.presentation.ui.adapter.CourseAdapter
 import com.neurospherex.utils.extensions.initiateStatusBar
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private lateinit var activityHomeBinding: ActivityHomeBinding
     private lateinit var courseAdapter: CourseAdapter
+    private lateinit var drawerToggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +27,15 @@ class HomeActivity : AppCompatActivity() {
         activityHomeBinding.lifecycleOwner = this
         initiateStatusBar()
         setupRecyclerView()
+        setupNavigationDrawer()
+
+        activityHomeBinding.userProfile.setOnClickListener {
+            openDrawer()
+        }
+
+        activityHomeBinding.navView.getHeaderView(0).findViewById<View>(R.id.backButton).setOnClickListener {
+            closeDrawer()
+        }
     }
 
     private fun setupRecyclerView() {
@@ -47,5 +62,41 @@ class HomeActivity : AppCompatActivity() {
             layoutManager = LinearLayoutManager(this@HomeActivity)
             adapter = courseAdapter
         }
+    }
+
+    private fun setupNavigationDrawer() {
+        drawerToggle = ActionBarDrawerToggle(this, activityHomeBinding.drawerLayout, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        activityHomeBinding.drawerLayout.addDrawerListener(drawerToggle)
+        drawerToggle.syncState()
+        activityHomeBinding.navView.setNavigationItemSelectedListener(this)
+        activityHomeBinding.drawerLayout.addDrawerListener(object : DrawerLayout.DrawerListener {
+            override fun onDrawerSlide(drawerView: View, slideOffset: Float) {}
+            override fun onDrawerOpened(drawerView: View) {}
+            override fun onDrawerClosed(drawerView: View) {}
+            override fun onDrawerStateChanged(newState: Int) { drawerToggle.syncState() }
+        })
+    }
+
+    private fun openDrawer() {
+        if (!activityHomeBinding.drawerLayout.isDrawerOpen(activityHomeBinding.navView)) {
+            activityHomeBinding.drawerLayout.openDrawer(activityHomeBinding.navView)
+        }
+    }
+
+    private fun closeDrawer() {
+        if (activityHomeBinding.drawerLayout.isDrawerOpen(activityHomeBinding.navView)) {
+            activityHomeBinding.drawerLayout.closeDrawer(activityHomeBinding.navView)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.userProfile -> {}
+            R.id.settings -> {}
+            R.id.logout -> {}
+        }
+
+        activityHomeBinding.drawerLayout.closeDrawers()
+        return true
     }
 }
